6343149615ad532b8ed68d1cd223018e61b196ab
sudhir81 <strangebiker@gmail.com>
2025-09-08 16:47:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,70 +1,93 @@
+# -------------------------------
+# Provider
+# -------------------------------
+provider "azurerm" {
+  features {}
+}
 
+# -------------------------------
+# Resource Group
+# -------------------------------
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "rg-ansible-demo"
   location = var.location
 }
 
+# -------------------------------
+# Virtual Network
+# -------------------------------
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-sandbox"
-  address_space       = ["10.1.0.0/16"]
+  name                = "vnet-ansible"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# -------------------------------
+# Subnet
+# -------------------------------
 resource "azurerm_subnet" "subnet" {
-  name                 = "snet-sandbox"
+  name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.1.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
+# -------------------------------
+# Network Security Group
+# -------------------------------
 resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg-sandbox"
+  name                = "nsg-ansible"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "Allow-RDP"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = var.allowed_cidr
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-WinRM"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "5985"
-    source_address_prefix      = var.allowed_cidr
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-HTTP"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "0.0.0.0/0"
-    destination_address_prefix = "*"
-  }
 }
 
-resource "azurerm_subnet_network_security_group_association" "snet_nsg" {
-  subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+# NSG Rules
+resource "azurerm_network_security_rule" "winrm" {
+  name                        = "Allow-WinRM"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "5985"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = azurerm_resource_group.rg.name
 }
 
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "Allow-RDP"
+  priority                    = 120
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "icmp" {
+  name                        = "Allow-ICMP"
+  priority                    = 130
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Icmp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = azurerm_resource_group.rg.name
+}
+
+# -------------------------------
+# Public IPs
+# -------------------------------
 resource "azurerm_public_ip" "dc01_pip" {
   name                = "pip-dc01"
   location            = azurerm_resource_group.rg.location
@@ -81,13 +104,16 @@ resource "azurerm_public_ip" "ws01_pip" {
   sku                 = "Standard"
 }
 
+# -------------------------------
+# Network Interfaces
+# -------------------------------
 resource "azurerm_network_interface" "dc01_nic" {
   name                = "nic-dc01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "ipconfig1"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.dc01_pip.id
@@ -100,20 +126,34 @@ resource "azurerm_network_interface" "ws01_nic" {
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "ipconfig1"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.ws01_pip.id
   }
 }
 
+# Associate NSG with NICs
+resource "azurerm_network_interface_security_group_association" "dc01_nic_nsg" {
+  network_interface_id      = azurerm_network_interface.dc01_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_network_interface_security_group_association" "ws01_nic_nsg" {
+  network_interface_id      = azurerm_network_interface.ws01_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# -------------------------------
+# Windows VMs
+# -------------------------------
 resource "azurerm_windows_virtual_machine" "dc01" {
   name                  = "DC01"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
-  size                  = var.vm_size
-  admin_username        = var.vm_admin_username
-  admin_password        = var.vm_admin_password
+  size                  = "Standard_B2s"
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
   network_interface_ids = [azurerm_network_interface.dc01_nic.id]
 
   os_disk {
@@ -133,9 +173,9 @@ resource "azurerm_windows_virtual_machine" "ws01" {
   name                  = "WS01"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
-  size                  = var.vm_size
-  admin_username        = var.vm_admin_username
-  admin_password        = var.vm_admin_password
+  size                  = "Standard_B2s"
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
   network_interface_ids = [azurerm_network_interface.ws01_nic.id]
 
   os_disk {
@@ -151,26 +191,60 @@ resource "azurerm_windows_virtual_machine" "ws01" {
   }
 }
 
+# -------------------------------
+# WinRM Extensions
+# -------------------------------
 resource "azurerm_virtual_machine_extension" "dc01_winrm_ext" {
-  name                 = "enable-winrm-dc01"
+  name                 = "EnableWinRM"
   virtual_machine_id   = azurerm_windows_virtual_machine.dc01.id
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.10"
 
-  settings = jsonencode({
-    commandToExecute = "powershell.exe -Command \"Enable-PSRemoting -Force; Set-Item -Path WSMan:\\localhost\\Service\\AllowUnencrypted -Value $true; Set-Item -Path WSMan:\\localhost\\Service\\Auth\\Basic -Value $true; New-NetFirewallRule -DisplayName 'Allow-WinRM' -Direction Inbound -LocalPort 5985 -Protocol TCP -Action Allow -Enabled True; New-NetFirewallRule -DisplayName 'Allow-HTTP' -Direction Inbound -LocalPort 80 -Protocol TCP -Action Allow -Enabled True; New-NetFirewallRule -Name 'Allow-ICMPv4-In' -Protocol ICMPv4 -IcmpType Any -Action Allow -Direction Inbound -Enabled True\""
-  })
+  settings = <<SETTINGS
+{
+  "commandToExecute": "powershell -ExecutionPolicy Unrestricted -Command \"
+    Set-Item WSMan:\\localhost\\Service\\AllowUnencrypted -Value True;
+    Set-Item WSMan:\\localhost\\Service\\Auth\\Basic -Value True;
+    Restart-Service WinRM -Force
+  \""
+}
+SETTINGS
+
+  depends_on = [
+    azurerm_windows_virtual_machine.dc01
+  ]
 }
 
 resource "azurerm_virtual_machine_extension" "ws01_winrm_ext" {
-  name                 = "enable-winrm-ws01"
+  name                 = "EnableWinRM"
   virtual_machine_id   = azurerm_windows_virtual_machine.ws01.id
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.10"
 
-  settings = jsonencode({
-    commandToExecute = "powershell.exe -Command \"Enable-PSRemoting -Force; Set-Item -Path WSMan:\\localhost\\Service\\AllowUnencrypted -Value $true; Set-Item -Path WSMan:\\localhost\\Service\\Auth\\Basic -Value $true; New-NetFirewallRule -DisplayName 'Allow-WinRM' -Direction Inbound -LocalPort 5985 -Protocol TCP -Action Allow -Enabled True; New-NetFirewallRule -DisplayName 'Allow-HTTP' -Direction Inbound -LocalPort 80 -Protocol TCP -Action Allow -Enabled True; New-NetFirewallRule -Name 'Allow-ICMPv4-In' -Protocol ICMPv4 -IcmpType Any -Action Allow -Direction Inbound -Enabled True\""
-  })
+  settings = <<SETTINGS
+{
+  "commandToExecute": "powershell -ExecutionPolicy Unrestricted -Command \"
+    Set-Item WSMan:\\localhost\\Service\\AllowUnencrypted -Value True;
+    Set-Item WSMan:\\localhost\\Service\\Auth\\Basic -Value True;
+    Restart-Service WinRM -Force
+  \""
+}
+SETTINGS
+
+  depends_on = [
+    azurerm_windows_virtual_machine.ws01
+  ]
+}
+
+# -------------------------------
+# Outputs
+# -------------------------------
+output "dc01_public_ip" {
+  value = azurerm_public_ip.dc01_pip.ip_address
+}
+
+output "ws01_public_ip" {
+  value = azurerm_public_ip.ws01_pip.ip_address
 }
